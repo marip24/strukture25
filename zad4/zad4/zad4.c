1a98b0ba37214da33a39d0bd263517e23f9cb544
marip24 <marip>
2026-03-22 21:59:37 +0100

    4. zadatak

--- zad4/zad4/zad4.c
+++ zad4/zad4/zad4.c
@@ -4,6 +4,7 @@ Napomena: Eksponenti u datoteci nisu nužno sortirani.*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct _poly* Position;
 typedef struct _poly {
 	int coeff;
